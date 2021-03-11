e576b9cf6a7197515b27ae7e42d006bb6c15e6d9
moin-ghadiyali-aot <moin.ghadiyali@algoocean.com>
2021-03-11 16:32:06 +0530

    changes

--- controllers/data1.controller.ts
+++ controllers/data1.controller.ts
@@ -1026,20 +1026,20 @@ export default class Data1Controller {
                   } else {
                     DataEntry.aggregate([
                       {
+                        $search: {
+                          'text': {
+                            'query': req.query.search,
+                            'path': ['respondentName', 'appelentName', 'judges', 'decidedDate', 'importantPoints', 'importantPointsHindi', 'importantPointsMarathi', 'importantPointsGujrati', 'headNote', 'headNoteHindi', 'headNoteGujrati', 'headNoteMarathi', 'result', 'resultHindi', 'resultMarathi', 'resultGujrati']
+                          }
+                        }
+                      },
+                      {
                         "$addFields": {
                           "decidedDate": {
                             "$convert": {
                               "input": "$decidedDate",
                               "to": "date"
                             }
-                          }
-                        }
-                      },
-                      {
-                        $search: {
-                          'text': {
-                            'query': req.query.search,
-                            'path': ['respondentName', 'appelentName', 'judges', 'decidedDate', 'importantPoints', 'importantPointsHindi', 'importantPointsMarathi', 'importantPointsGujrati', 'headNote', 'headNoteHindi', 'headNoteGujrati', 'headNoteMarathi', 'result', 'resultHindi', 'resultMarathi', 'resultGujrati']
                           }
                         }
                       },
