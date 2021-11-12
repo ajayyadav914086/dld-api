0b045bf37b7e6f6671d43bf66d61a17d39499ff3
ajay yadav <taxpertssystempvtltd@instance-1.us-central1-a.c.symbolic-cinema-322104.internal>
2021-11-12 13:10:52 +0000

    docker file added

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import AdminRoute from './routes/admin.route';
 import DataRoute from './routes/data.route';
 import OTPRoute from './routes/otp.route';
 import UserRoute from './routes/user.route';
-var port = process.env.PORT || 3005;
+var port = process.env.PORT || 4200;
 import bodyParser = require('body-parser');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
