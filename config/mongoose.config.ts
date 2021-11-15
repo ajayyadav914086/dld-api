bd8ed3c857946eb0ac624c441cea72054df9f3f2
ajay yadav <ajayyadav@ajays-MacBook-Air.local>
2021-11-15 13:55:47 +0530

    change in connect string

--- config/mongoose.config.ts
+++ config/mongoose.config.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-let dev_db_url = 'mongodb+srv://ajay7722:ajay2020@cluster0.qkzux.gcp.mongodb.net/Dld?retryWrites=true&w=majority';
+let dev_db_url = 'mongodb+srv://dld:dld@2020!@cluster0.zh7f5.mongodb.net/dev?retryWrites=true&w=majority';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
